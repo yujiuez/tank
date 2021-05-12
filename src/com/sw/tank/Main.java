81975203c392c40b385e45e5c7de591fb5d2bd3d
DESKTOP-DV0G086\yujie <yujiez@outlook.com>
2021-05-12 23:30:10 +0800

    tank-v1.21 使用配置文件的方法将一些可配置的参数单独列出来

--- src/com/sw/tank/Main.java
+++ src/com/sw/tank/Main.java
@@ -8,9 +8,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
       TankFrame tf = new TankFrame();
+      int initTankCount = Integer.parseInt((String)PropertyMgr.get("initTankCount"));
 
       //初始化敌方坦克
-      for(int i=0;i<5;i++){
+      for(int i=0;i<initTankCount;i++){
           tf.tanks.add(new Tank(50+ i*30,200,Dir.DOWN,tf,Group.BAD));
       }
       while(true){
