0be15a6eb9e95831410a49b318d492c3cf635c14
DESKTOP-DV0G086\yujie <yujiez@outlook.com>
2021-05-12 20:54:28 +0800

    tank-v1.16 敌人坦克随机移动方向

--- src/com/sw/tank/Tank.java
+++ src/com/sw/tank/Tank.java
@@ -101,10 +101,17 @@ public class Tank {
                 default:
                     break;
             }
-            if(random.nextInt(10) > 8) this.fire();
+            if(this.group == Group.BAD && random.nextInt(100) > 95)
+                this.fire();
+            if(this.group == Group.BAD && random.nextInt(100) > 95)
+                randomDir();
         }
 
 
+    }
+
+    private void randomDir() {
+        this.dir = Dir.values()[random.nextInt(4)];
     }
 
     public void fire() {
